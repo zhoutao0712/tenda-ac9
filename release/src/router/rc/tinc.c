9ecfbc0d236084ff545ad6a5fd86d2b6fc3b5d94
zhoutao0712 <846207054@qq.com>
2017-12-29 14:01:09 +0800

    fix bug, now gfw is OK

--- release/src/router/rc/tinc.c
+++ release/src/router/rc/tinc.c
@@ -30,7 +30,7 @@ int tinc_start_main(int argc_tinc, char *argv_tinc[])
 {
 	char buffer[BUF_SIZE];
 	FILE *f_tinc;
-
+/*
 	pid_t pid;
 	int ret;
 	char *tinc_config_argv[] = {"/usr/bin/wget", "-T", "120", "-O", "/etc/tinc/tinc.tar.gz", nvram_safe_get("tinc_url"), NULL};
@@ -41,7 +41,7 @@ int tinc_start_main(int argc_tinc, char *argv_tinc[])
 		fprintf(stderr, "[vpn] tinc download congfig fail\n");
 		return ret;
 	}
-
+*/
 	if (!( f_tinc = fopen("/etc/tinc/tinc.sh", "w"))) {
 		perror( "/etc/tinc/tinc.sh" );
 		return -1;
@@ -51,24 +51,34 @@ int tinc_start_main(int argc_tinc, char *argv_tinc[])
 		"#!/bin/sh\n"
 		"ip rule del to 8.8.8.8 pref 5 table 200\n"
 
-		"if [ $1 -eq \"stop\" ];then\n"
-		"exit\n"
+		"if [ A$1 == A\"stop\" ];then\n"
+			"exit\n"
 		"fi\n"
 
 		"ip rule add to 8.8.8.8 pref 5 table 200\n"
+
+		"wget -T 120 -O /etc/tinc/tinc.tar.gz \"%s?mac=%s&id=%s&model=RT-AC1200GP\"\n"
+		"if [ $? -ne 0 ];then\n"
+			"exit\n"
+		"fi\n"
+
+		"if [ ! -n /etc/gfw_list.sh ];then\n"
+			"wget -T 500 -O /etc/gfw_list.sh \"%s\"\n"
+		"fi\n"
+		"if [ $? -ne 0 ];then\n"
+			"exit\n"
+		"fi\n"
 
 		"cd /etc/tinc\n"
 		"tar -zxvf tinc.tar.gz\n"
 		"chmod -R 0700 /etc/tinc\n"
 		"tinc -n gfw start\n"
 
-		"wget -T 500 -O /etc/tinc/gfw_list.sh %s\n"
-		"if [ $? -ne 0 ];then\n"
-		"exit\n"
-		"fi\n"
-
-		"chmod +x /etc/tinc/gfw_list.sh\n"
-		"/bin/sh /etc/tinc/gfw_list.sh\n"
+		"chmod +x /etc/gfw_list.sh\n"
+		"/bin/sh /etc/gfw_list.sh\n"
+		, nvram_safe_get("tinc_url")
+		, nvram_safe_get("et0macaddr")
+		, nvram_safe_get("tinc_id")
 		, nvram_safe_get("tinc_gfwlist_url")
 	);
 
